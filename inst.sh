c18ac7c081d518900f408f0ce66d02e7aaad9340
Игорь Белоусов <radmyn@yandex.ru>
2022-02-25 16:29:05 +0300

    cnange file inst

--- inst.sh
+++ inst.sh
@@ -1,8 +1,12 @@
 cd ~
+apt-get update
+apt-get install -y build-essential
 wget https://github.com/z3APA3A/3proxy/archive/0.9.3.tar.gz
 tar xzf 0.9.3.tar.gz
 cd ~/3proxy-0.9.3
-sudo make -f Makefile.Linux
+make -f Makefile.Linux
 mkdir /etc/3proxy
+mkdir /usr/local/3proxy
 cd ~/3proxy-0.9.3/bin
 cp 3proxy /usr/bin/
+cp 3proxy /usr/local/3proxy
